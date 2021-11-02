d2fe19c82649ad923426b940ab822af47ee25909
Ihor.Tarkhan <ihor.tarkhan@lafox.net>
2021-11-02 16:01:17 +0200

    thymeleaf added to project

---
+++ src/main/java/ua/knu/csc/iss/primitivebank/TestRestController.java
@@ -0,0 +1,14 @@
+package ua.knu.csc.iss.primitivebank;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TestRestController {
+    @GetMapping("/test-rest")
+    public Object getTest() {
+        return Map.of("name", "eee", "age", 6);
+    }
+}
