dd1bd6e68b20f8a52427d2535ef515121493ca05
Ihor.Tarkhan <ihor.tarkhan@lafox.net>
2021-11-02 15:33:50 +0200

    init project

---
+++ src/main/java/ua/knu/csc/iss/primitivebank/PrimitiveBankApplication.java
@@ -0,0 +1,11 @@
+package ua.knu.csc.iss.primitivebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimitiveBankApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrimitiveBankApplication.class, args);
+    }
+}
---
+++ src/test/java/ua/knu/csc/iss/primitivebank/PrimitiveBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.knu.csc.iss.primitivebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimitiveBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
