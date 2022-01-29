3b7b9559932c57ab9d4f91d6e08afbbe3055fc71
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-01-29 12:43:45 +0200

    rest request log

--- src/main/java/ua/knu/csc/iss/mvpbank/configuration/FilterConfiguration.java
+++ src/main/java/ua/knu/csc/iss/mvpbank/configuration/FilterConfiguration.java
@@ -1,25 +1,19 @@
 package ua.knu.csc.iss.mvpbank.configuration;
 
 import ch.qos.logback.access.servlet.TeeFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+import java.util.Collections;
 
 @Configuration
 public class FilterConfiguration {
-
-    @Bean
-    public FilterRegistrationBean requestResponseFilter() {
-
-        final FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
-        TeeFilter filter = new TeeFilter();
-        filterRegBean.setFilter(filter);
-        filterRegBean.setUrlPatterns(List.of("*"));
-        filterRegBean.setName("Request Response Filter");
-        filterRegBean.setAsyncSupported(Boolean.TRUE);
-        return filterRegBean;
-    }
+  @Bean
+  public FilterRegistrationBean<TeeFilter> requestResponseFilter() {
+    var filterRegBean = new FilterRegistrationBean<>(new TeeFilter());
+    filterRegBean.setUrlPatterns(Collections.singleton("*"));
+    filterRegBean.setAsyncSupported(Boolean.TRUE);
+    return filterRegBean;
+  }
 }
