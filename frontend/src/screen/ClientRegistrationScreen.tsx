0bf0478b8acd8f2d244e0908b82f3725f77a7c2d
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-01-31 22:09:07 +0200

    formik screen rewrite

--- frontend/src/screen/ClientRegistrationScreen.tsx
+++ frontend/src/screen/ClientRegistrationScreen.tsx
@@ -1,7 +1,9 @@
 import React, { ReactElement } from "react";
 import {
   Box,
+  Button,
   FormControl,
+  FormHelperText,
   IconButton,
   InputLabel,
   OutlinedInput,
@@ -10,58 +12,135 @@ import {
 } from "@mui/material";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { Formik } from "formik";
+import { makeStyles } from "@mui/styles";
+import { FormicErrors } from "../util/FormicUtil";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: "500px",
+    marginInline: "auto",
+    marginTop: "100px",
+    rowGap: "7px",
+  },
+});
 
 export const ClientRegistrationScreen = (): ReactElement => {
-  const [email, setEmail] = React.useState<string>("");
-  const [password, setPassword] = React.useState<string>("");
-  const [confirmPassword, setConfirmPassword] = React.useState<string>("");
-  const [showPassword, setShowPassword] = React.useState<boolean>(false);
+  const classes = useStyles();
 
+  const registrationLabel = "Registration";
   const emailLabel = "Email";
   const passwordLabel = "Password";
   const confirmPasswordLabel = "Confirm password";
-  const registrationLabel = "Registration";
+  const submitLabel = "Submit";
+
+  const required = "Required";
+  const invalidEmailAddress = "Invalid email address";
+  const chooseLongerPassword = "Choose a longer password, at less 8 characters";
+  const passwordsNotMatch = "Passwords do not match";
+
+  type FormikData = {
+    email: string;
+    password: string;
+    confirmPassword: string;
+    isShowPassword: boolean;
+  };
+
+  const initDate: FormikData = {
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isShowPassword: false,
+  };
+
+  const validate = (values: FormikData) => {
+    const errors: FormicErrors<FormikData> = {};
+    if (!values.email) {
+      errors.email = required;
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = invalidEmailAddress;
+    }
+    if (!values.password) {
+      errors.password = required;
+    } else if (values.password.length < 8) {
+      errors.password = chooseLongerPassword;
+    }
+    if (!errors.password) {
+      if (!values.confirmPassword) {
+        errors.confirmPassword = required;
+      } else if (values.confirmPassword !== values.password) {
+        errors.confirmPassword = passwordsNotMatch;
+      }
+    }
+    return errors;
+  };
+
+  const submit = (values: FormikData) => {
+    console.log(values);
+  };
 
   return (
-    <Box
-      display={"flex"}
-      flexDirection={"column"}
-      maxWidth={"500px"}
-      marginX={"auto"}
-      marginTop={"100px"}
-      rowGap={"20px"}
-    >
-      <Typography variant={"h3"}>{registrationLabel}</Typography>
-      <TextField
-        variant={"outlined"}
-        label={emailLabel}
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <FormControl variant={"outlined"}>
-        <InputLabel htmlFor={"password"}>{passwordLabel}</InputLabel>
-        <OutlinedInput
-          id={"password"}
-          type={showPassword ? "text" : "password"}
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          endAdornment={
-            <IconButton
-              onClick={() => setShowPassword((prevState) => !prevState)}
+    <Box className={classes.root}>
+      <Typography variant={"h4"} mb={"20px"}>
+        {registrationLabel}
+      </Typography>
+      <Formik initialValues={initDate} validate={validate} onSubmit={submit}>
+        {({ values, errors, touched, handleSubmit, setFieldValue }) => (
+          <>
+            <TextField
+              variant={"outlined"}
+              label={emailLabel}
+              value={values.email}
+              onChange={(event) => setFieldValue("email", event.target.value)}
+              error={!!(touched.email && errors.email)}
+              helperText={(touched.email && errors.email) || " "}
+            />
+            <FormControl
+              variant={"outlined"}
+              error={!!(touched.password && errors.password)}
             >
-              {showPassword ? <VisibilityOff /> : <Visibility />}
-            </IconButton>
-          }
-          label={passwordLabel}
-        />
-      </FormControl>
-      <TextField
-        variant={"outlined"}
-        label={confirmPasswordLabel}
-        type={showPassword ? "text" : "password"}
-        value={confirmPassword}
-        onChange={(event) => setConfirmPassword(event.target.value)}
-      />
+              <InputLabel htmlFor={"password"}>{passwordLabel}</InputLabel>
+              <OutlinedInput
+                id={"password"}
+                label={passwordLabel}
+                type={values.isShowPassword ? "text" : "password"}
+                value={values.password}
+                onChange={(event) =>
+                  setFieldValue("password", event.target.value)
+                }
+                endAdornment={
+                  <IconButton
+                    onClick={() =>
+                      setFieldValue("isShowPassword", !values.isShowPassword)
+                    }
+                  >
+                    {values.isShowPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                }
+              />
+              <FormHelperText error id="accountId-error">
+                {(touched.password && errors.password) || " "}
+              </FormHelperText>
+            </FormControl>
+            <TextField
+              variant={"outlined"}
+              label={confirmPasswordLabel}
+              type={values.isShowPassword ? "text" : "password"}
+              value={values.confirmPassword}
+              onChange={(event) =>
+                setFieldValue("confirmPassword", event.target.value)
+              }
+              error={!!(touched.email && errors.confirmPassword)}
+              helperText={(touched.email && errors.confirmPassword) || " "}
+            />
+            <Button variant={"contained"} onClick={() => handleSubmit()}>
+              {submitLabel}
+            </Button>
+          </>
+        )}
+      </Formik>
     </Box>
   );
 };
