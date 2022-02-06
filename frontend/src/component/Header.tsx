db10f9e548b6f69fbed1c690b3f41322bf5b51f8
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-02-06 12:55:45 +0200

    refactored Header margin-padding

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -6,17 +7,15 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import { useState } from "react";
 
 const pages = ["Products", "Pricing", "Blog"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
-export const ResponsiveAppBar = () => {
+export const Header = () => {
   const [anchorElNav, setAnchorElNav] = useState<HTMLElement | null>(null);
   const [anchorElUser, setAnchorElUser] = useState<HTMLElement | null>(null);
 
@@ -36,13 +35,13 @@ export const ResponsiveAppBar = () => {
   };
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
+    <>
+      <AppBar>
+        <Toolbar>
           <Typography
-            variant="h6"
+            variant={"h6"}
             noWrap
-            component="div"
+            component={"div"}
             sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
           >
             LOGO
@@ -50,44 +49,36 @@ export const ResponsiveAppBar = () => {
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
+              size={"large"}
+              aria-label={"account of current user"}
+              aria-controls={"menu-appbar"}
+              aria-haspopup={"true"}
               onClick={handleOpenNavMenu}
-              color="inherit"
+              color={"inherit"}
             >
               <MenuIcon />
             </IconButton>
             <Menu
-              id="menu-appbar"
+              id={"menu-appbar"}
               anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
+              anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
               keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
+              transformOrigin={{ vertical: "top", horizontal: "left" }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
+              sx={{ display: { xs: "block", md: "none" } }}
             >
               {pages.map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
+                  <Typography textAlign={"center"}>{page}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
           <Typography
-            variant="h6"
+            variant={"h6"}
             noWrap
-            component="div"
+            component={"div"}
             sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
           >
             LOGO
@@ -105,36 +96,34 @@ export const ResponsiveAppBar = () => {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
+            <Tooltip title={"Open settings"}>
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar
+                  alt={"Remy Sharp"}
+                  src={"/static/images/avatar/2.jpg"}
+                />
               </IconButton>
             </Tooltip>
             <Menu
               sx={{ mt: "45px" }}
-              id="menu-appbar"
+              id={"menu-appbar"}
               anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
+              anchorOrigin={{ vertical: "top", horizontal: "right" }}
               keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
+              transformOrigin={{ vertical: "top", horizontal: "right" }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
               {settings.map((setting) => (
                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+                  <Typography textAlign={"center"}>{setting}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
         </Toolbar>
-      </Container>
-    </AppBar>
+      </AppBar>
+      <Toolbar style={{ marginBottom: 15 }} />
+    </>
   );
 };
--- frontend/src/screen/ClientHomeScreen.tsx
+++ frontend/src/screen/ClientHomeScreen.tsx
@@ -1,20 +1,20 @@
 import React, { ReactElement } from "react";
-import { Box, Link } from "@mui/material";
+import { Container, Link } from "@mui/material";
 import {
   CLIENT_LOGIN_ROUTE,
   CLIENT_REGISTRATION_ROUTE,
 } from "../constant/route";
-import { ResponsiveAppBar } from "../component/ResponsiveAppBar";
+import { Header } from "../component/Header";
 
 export const ClientHomeScreen = (): ReactElement => {
   return (
-    <Box>
-      <ResponsiveAppBar />
+    <Container maxWidth={false}>
+      <Header />
       Home Screen
       <br />
       <Link href={CLIENT_LOGIN_ROUTE}>login</Link>
       <br />
       <Link href={CLIENT_REGISTRATION_ROUTE}>registr</Link>
-    </Box>
+    </Container>
   );
 };
