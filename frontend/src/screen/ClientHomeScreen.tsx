354c3c73ff04e31b6ecabb43f27aa4f3b40e891f
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-02-06 11:56:27 +0200

    init Header

--- frontend/src/screen/ClientHomeScreen.tsx
+++ frontend/src/screen/ClientHomeScreen.tsx
@@ -4,10 +4,12 @@ import {
   CLIENT_LOGIN_ROUTE,
   CLIENT_REGISTRATION_ROUTE,
 } from "../constant/route";
+import { ResponsiveAppBar } from "../component/ResponsiveAppBar";
 
 export const ClientHomeScreen = (): ReactElement => {
   return (
     <Box>
+      <ResponsiveAppBar />
       Home Screen
       <br />
       <Link href={CLIENT_LOGIN_ROUTE}>login</Link>
