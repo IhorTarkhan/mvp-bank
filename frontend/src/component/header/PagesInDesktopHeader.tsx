7f748a41818469fd3a3deb1e59c5cc5c526e69a9
ihor.tarkhan <ihor.tarkhan.working@gmail.com>
2022-05-12 21:28:37 +0300

    languages in header component

--- frontend/src/component/header/PagesInDesktopHeader.tsx
+++ frontend/src/component/header/PagesInDesktopHeader.tsx
@@ -1,20 +1,7 @@
 import * as React from "react";
 import { ReactElement } from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
-import { AccountCircle } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
-import { MAIN_APP_COLOR, TEXT_ON_MAIN_COLOR } from "../../constant/colors";
-import { Logo } from "../Logo";
-import { AreaWithSideMenu } from "../AreaWithSideMenu";
-import { AriaWithPopupMenu } from "../AriaWithPopupMenu";
-import { useNavigate } from "react-router-dom";
-import { CLIENT_HOME_ROUTE } from "../../constant/route";
-import { LanguagesInHeader } from "./LanguagesInHeader";
-import { SettingsInHeader } from "./SettingsInHeader";
+import { TEXT_ON_MAIN_COLOR } from "../../constant/colors";
 
 type Props = {
   pages: { name: string; onClick: () => void; icon?: ReactElement }[];
