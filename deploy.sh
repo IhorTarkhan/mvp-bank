5cbda5b3f0f1be0cccf6ddbfd4d019ed78e5e310
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-05-27 11:20:34 +0300

    remove tests from dependency

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,8 @@ mv ".env" ".env.prod"
 mv ".env.tmp" ".env"
 ssh mvp_bank@ihor-tarkhan.com "rm -fr frontend/build"
 scp -r build mvp_bank@ihor-tarkhan.com:~/frontend/
+rm -fr node_modules
+rm -fr build
 
 cd ..
 
@@ -24,7 +26,6 @@ ssh mvp_bank@ihor-tarkhan.com '[ -z "$(sudo lsof -t -i:8080)" ] || sudo kill "$(
 ssh mvp_bank@ihor-tarkhan.com "rm -f backend/MvpBank.jar"
 scp target/MvpBank.jar mvp_bank@ihor-tarkhan.com:~/backend
 ssh mvp_bank@ihor-tarkhan.com "java -jar ~/backend/MvpBank.jar --spring.config.additional-location=backend/external-application.properties" &
-slip 20
 
 cd ..
 
